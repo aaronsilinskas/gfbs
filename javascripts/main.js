96007868d2ac798673f5289aeb2dd1de9f60cbfa
aaron@mindwidgets.com <aaron@mindwidgets.com>
2016-10-12 22:55:04 -0700

    corrected click class

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 
 
-$('.experiment-header').click(function () {
+$('.exercise-header').click(function () {
     var $this = $(this);
     $this.next().slideToggle(100).promise().done(function () {
         $this.find('span').text(function (_, value) {
