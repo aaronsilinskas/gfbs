5426d5f24746dc3c37b90f6ae5f4636a95994e50
Aaron Silinskas <asilinskas@expedia.com>
2016-10-12 18:25:38 -0700

    WIP expand collapse working, need icons

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+
+
+$('.experiment-header').click(function () {
+    var $this = $(this);
+    $this.next().slideToggle(100).promise().done(function () {
+        $this.find('span').text(function (_, value) {
+            return value == '-' ? '+' : '-'
+        });
+    });
+});
